5da9e6d51dfdf7c82f669e35384f0c5053bf0f96
Regina1965 <149797487+Regina1965@users.noreply.github.com>
2024-02-12 15:52:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var progress = document.getElementById('progress');
 
 document.getElementById('accept').addEventListener('click', function() {
-    if(progress.value < 100) progress.value += 5;
+    if(progress.value < 100) progress.value += 10;
 }, false);
 
 document.getElementById('reject').addEventListener('click', function() {
-    progress.value -= 5;
+    progress.value -= 10;
 }, false);
